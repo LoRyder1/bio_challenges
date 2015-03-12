0e3e6e15c4ef91ac152ede45207a33f40656ad3b
Lovinder Pnag <lspnag@gmail.com>
2015-03-12 16:56:07 -0500

    adding more tests for task1

--- spec/task1_spec.rb
+++ spec/task1_spec.rb
@@ -38,12 +38,39 @@ end
 
 describe FileFastQ do 
 	let(:list) { ListFastQ.new }
-	list.find_fastq_files
-	list.get_percents
+	before { list.find_fastq_files }
+	before { list.get_percents }
 
-	describe '#'
+	
+	describe '#get_percent' do 
+		it "is defined" do 
+			expect(FileFastQ.method_defined?(:get_percent)).to be true
+		end
+		it "parses fastq file" do 
+			expect(list.files[0].file_by_line).to be_instance_of(Array)
+		end
+		it "after parses file_by_line should not be empty" do 
+			expect(list.files[0].file_by_line).to_not be_empty
+		end
+		it "parse_fastq should be defined" do
+			expect(FileFastQ.method_defined?(:parse_fastq)).to be true
+		end
+		it "after parsing all sequences array should not be empty" do
+			expect(list.files[0].all_seqs).to_not be_empty
+		end
+		it "after parsing expecting percent greater than 30 to be empty" do
+			expect(list.files[0].seqs_larger_30).to_not be_empty
+		end
+		it "expect seqs_larger_30 be less than all seqs" do 
+			expect(list.files[0].seqs_larger_30.count).to be < (list.files[0].all_seqs.count)
+		end
+	end
 
-
+	describe '#percent' do 
+		it "expects 2 arguments" do
+    	method(:percent).arity.should eq 2
+  	end
+	end
 
 end
 
